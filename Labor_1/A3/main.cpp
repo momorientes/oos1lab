7b1b5121b168e5ef1cac713bc7b8372ec415c531
Wolfram Ladurner <wolait01@hs-esslingen.de>
2015-03-22 17:03:58 +0100

    Header eingefügt

--- Labor_1/A3/main.cpp
+++ Labor_1/A3/main.cpp
@@ -1,3 +1,11 @@
+// Dateiname: main.cpp
+// Autoren: Wolfram Ladurner <wolait01@hs-esslingen.de>
+//          Moritz Frenzel <mofrit01@hs-esslingen.de>
+// Beschreibung: Labyrinth-generator
+// Datum: 2015/03/22 - 2015/03/22
+// Aufwand: 10 Min.
+// Modifikationen:
+
 #include <iostream>
 #include <curses.h>
 
