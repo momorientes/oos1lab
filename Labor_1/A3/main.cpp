09627bfd4dd836dde223faac88ece7ecb46d16a9
Wolfram Ladurner <wolait01@hs-esslingen.de>
2015-03-22 23:46:23 +0100

    Konsolen-Ein-/Ausgabe gefixt

--- Labor_1/A3/main.cpp
+++ Labor_1/A3/main.cpp
@@ -39,16 +39,14 @@ void initialisieren() {
 // Labyrinth auf dem Bildschirm ausgeben
 void drucken() {
 	// Console frei machen
-	system("clear");
-	// Labyrinth ausgeben
+	erase();
 
+	// Labyrinth ausgeben
     for(auto& row: labyrinth)
     {
-        cout << row;
+        printw(row);
     }
-
-
-
+	 refresh();
 }
 
 // Hilfsfunktion max
@@ -62,37 +60,43 @@ int min(int x, int y) {
 }
 
 // Durch Herumlaufen werden Wege im Labyrinth erzeugt
-// ASCII-Wert der Tasten: oben 72, links 75, rechts 77, unten 80
 void erzeugen() {
-	char c = 'x';
+	int c = 'x';
 	int posx = kSpalten / 2;
 	int posy = kZeilen / 2;
+
 	labyrinth[posy][posx] = ICH;
-    noecho();
-	drucken();
 	while (c != 'q') {
 		drucken();
-		cout << "Laufen mit Pfeiltasten. Beenden mit q." << endl;
+		// cout << "Laufen mit Pfeiltasten. Beenden mit q." << endl;
+		printw("Laufen mit Pfeiltasten. Beenden mit q.\n");
+
 		labyrinth[posy][posx] = WEG;
-		while( ERR == (c = getch()));
-		switch (int(c)) {
+		c = getch();
+		switch (c) {
 			// oben
-		case 72: posy = max(1, posy - 1); break;
+		case KEY_UP: posy = max(1, posy - 1); break;
 			// links
-		case 75: posx = max(1, posx - 1); break;
+		case KEY_LEFT: posx = max(1, posx - 1); break;
 			// rechts
-		case 77: posx = min(kSpalten - 2, posx + 1); break;
+		case KEY_RIGHT: posx = min(kSpalten - 2, posx + 1); break;
 			// unten
-		case 80: posy = min(kZeilen - 2, posy + 1); break;
+		case KEY_DOWN: posy = min(kZeilen - 2, posy + 1); break;
 			// q = quit
-		case 113: break;
+		case 'q': break;
 		}
 		labyrinth[posy][posx] = ICH;
 	}
 }
 
 int main() {
+	// curses-screen initialisieren
+	initscr();
+	keypad(stdscr, TRUE);
+
 	initialisieren();
 	drucken();
 	erzeugen();
+
+	endwin();
 }
