b470a17ffdfa59036e20eb6a737ce9efd787c518
momo <github@mail.hackathon.de>
2015-03-24 09:14:00 +0100

    that's it

--- Labor_1/A4/main.cpp
+++ Labor_1/A4/main.cpp
@@ -5,11 +5,11 @@ Spiel Tsching, Tschang, Tschong
 Regeln:
 Es gibt zwei Spieler: der Programmierer und der Computer.
 Beide Spieler waehlen gleichzeitig eines der drei Objekte
-Stein, Schere und Papier. Beide kˆnnen das gleiche Objekt w‰hlen.
-Der Spieler, der das m‰chtigere Objekt gew‰hlt hat gewinnt.
+Stein, Schere und Papier. Beide k√∂nnen das gleiche Objekt w√§hlen.
+Der Spieler, der das m√§chtigere Objekt gew√§hlt hat gewinnt.
 SCHERE kann einen STEIN nicht zerschneiden, d.h. STEIN ist 
-m‰chtiger als SCHERE. PAPIER wickelt STEIN ein, d.h. PAPIER ist 
-m‰chtiger als STEIN. SCHERE zerschneidet PAPIER, d. h. SCHERE 
+m√§chtiger als SCHERE. PAPIER wickelt STEIN ein, d.h. PAPIER ist 
+m√§chtiger als STEIN. SCHERE zerschneidet PAPIER, d. h. SCHERE 
 ist maechtiger als PAPIER
 
 */
@@ -20,17 +20,17 @@ ist maechtiger als PAPIER
 #include <iostream>
 using namespace std;
 
-// Aufz‰hlungstyp f¸r Stein etc.
+// Aufz√§hlungstyp f√ºr Stein etc.
 enum objectType { STEIN, SCHERE, PAPIER };
 
-// Struktur f¸r einen Spieler bestehend aus Name und Wahl 
+// Struktur f√ºr einen Spieler bestehend aus Name und Wahl 
 // des Spielers
 struct player {
 	char * name;
 	objectType choice;
 };
 
-// Variable f¸r den Namen des Spielers 
+// Variable f√ºr den Namen des Spielers 
 char name[15];
 
 // Name des Spielers eingeben
@@ -38,9 +38,10 @@ char * enterName(char str[])
 {
     cout << "Namen eingeben: ";
     cin >> str;
+    return str;
 }
 
-// Den Computer zuf‰llig waehlen lassen. 
+// Den Computer zuf√§llig waehlen lassen. 
 // Nutzen Sie srand(...) und rand().
 objectType randomChoice()
 {
@@ -51,7 +52,7 @@ objectType randomChoice()
     return choice;
 }
 
-// Die Wahl von STEIN etc. als String zur¸ckgeben lassen
+// Die Wahl von STEIN etc. als String zur√ºckgeben lassen
 char * object2str(objectType o)
 {
     switch(o)
@@ -65,7 +66,7 @@ char * object2str(objectType o)
 // Einen Text mit dem Namen des Spielers und seiner Wahl ausgeben
 void showPlayer(player p)
 {
-    cout << "Spieler: " << p.name << "Wahl: " << p.choice << endl;
+    cout << "Spieler: " << p.name << " Wahl: " << p.choice << endl;
 }
 
 // Die Wahl des Spielers abfragen
@@ -75,17 +76,18 @@ objectType enterChoice()
    char input[7];
    cout << "schere, stein, oder papier? ";
    cin >> input;
-   if(input == "schere")
+   if(strcmp(input,"schere") == 0)
    {
        choice = SCHERE;
-   } else if(input == "stein")
+   } else if(strcmp(input, "stein") == 0)
    {
        choice = STEIN;
-   } else if (input == "papier")
+   } else if (strcmp(input, "papier") == 0)
    {
        choice = PAPIER;
    } else {
-       exit(0);
+       cout << "Unknown choice!" << endl;
+       exit(1);
    }
    return choice;
 }
@@ -94,27 +96,60 @@ objectType enterChoice()
 objectType winningObject(objectType obj1, objectType obj2)
 {
 
+    if(obj1 == SCHERE)
+    {
+        if(obj2 == STEIN)
+        {
+            return STEIN;
+        }
+        if(obj2 == PAPIER)
+        {
+            return SCHERE;
+        }
+    }
 
+    if(obj1 == STEIN)
+    {
+        if(obj2 == SCHERE)
+        {
+            return STEIN;
+        }
+        if(obj2 == PAPIER)
+        {
+            return PAPIER;
+        }
+    }
 
-
-
+    if(obj1 == PAPIER)
+    {
+        if(obj2 == SCHERE)
+        {
+            return SCHERE;
+        }
+        if(obj2 == STEIN)
+        {
+            return PAPIER;
+        }
+    }
 
 }
 
 // Ausgeben, wer gewonnen hat
 void showWinner(player p1, player p2)
 {
+    if(p1.choice == p2.choice)
+    {
+        cout << "unentschieden" << endl;
+        exit(0);
+    }
+    objectType winner = winningObject(p1.choice, p2.choice);
 
-
-
-
-
-
-
-
-
-
-
+    if(winner == p2.choice)
+    {
+        cout << "Gewonnen!" << endl;
+    } else {
+        cout << "Verloren!" << endl;
+    }
 
 }
 
