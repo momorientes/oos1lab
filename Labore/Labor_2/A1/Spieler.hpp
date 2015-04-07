36495c5a666a1b9fa3091bfb8a17cf50366c9463
Wolfram Ladurner <wolait01@hs-esslingen.de>
2015-04-07 14:18:00 +0200

    Klassenstruktur Spieler hinzugefügt, Makefile erstellt

---
+++ Labore/Labor_2/A1/Spieler.hpp
@@ -0,0 +1,35 @@
+// Datei Spieler.hpp
+#pragma once
+
+#include "Position.hpp"
+#include "Spieler.hpp"
+#include "MyString.hpp"
+
+class Spieler 
+{
+	MyString* name;           // Name des Spielers
+	Position *pos;            // Position des Spielers
+	int muenzen;              // Anzahl bereits gesammelter Münzen
+
+public:
+	// Kopierkonstruktoren
+	Spieler(const MyString& str);
+	Spieler(const char* str);
+
+	// Position lesen/setzen
+	Positon getPos();
+	void setPos(Position& pos);
+	// Richtung setzen
+	void setRichtung(Richtung dir);
+	// Anzahl Münzen lesen
+	int getMuenzen();
+	// Anzahl Münzen inkrementieren
+	void plusMuenze();
+	// Schritt im Labyrinth machen
+	Spieler& schritt(Labyrinth& lab);
+	// Orientieren im Labyrinth
+	void oreintieren(Labyrinth& lab);
+
+	void spielen();
+};
+
