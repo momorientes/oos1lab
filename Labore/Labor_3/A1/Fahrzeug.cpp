82d1439533e3113593828b0ded1c63925033642c
Wolfram Ladurner <wolait01@hs-esslingen.de>
2015-04-21 22:38:28 +0200

    initial commit Labor 3.1 (copy of Hausaufgabe 3.5)

---
+++ Labore/Labor_3/A1/Fahrzeug.cpp
@@ -0,0 +1,14 @@
+#include <iomanip>
+#include "Fahrzeug.hpp"
+
+Fahrzeug::Fahrzeug(const char* kz): kz(kz), vin(++zaehler), km(0.0) {};
+
+void Fahrzeug::fahren(double km) {
+	this->km += km;
+}
+
+std::ostream& operator<<(std::ostream &stream, const Fahrzeug &fahrzeug) {
+	return stream << "kz = " << fahrzeug.kz 
+		<< "  VIN = " << fahrzeug.vin
+		<< "  km = " << std::setprecision(1) << std::fixed << fahrzeug.km;
+}
---
+++ Labore/Labor_3/A1/main.cpp
@@ -0,0 +1,16 @@
+#include "Fahrzeug.hpp"
+
+using namespace std;
+
+int Fahrzeug::zaehler = 0;
+
+int main() {
+	Fahrzeug f1("ES - H 123");
+	cout << f1 << endl;
+	f1.fahren(101.5);
+	cout << f1 << endl;
+	Fahrzeug f2("ES - M 4711");
+	f2.fahren(10.57);
+	cout << f2 << endl;
+	return 0;
+}
