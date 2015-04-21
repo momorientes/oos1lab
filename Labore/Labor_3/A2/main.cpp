305820aaa918a7c64d5c850a2b1004fd97d7b6e5
Wolfram Ladurner <wolait01@hs-esslingen.de>
2015-04-21 22:20:35 +0200

    initial commit Labor 3

---
+++ Labore/Labor_3/A2/main.cpp
@@ -0,0 +1,20 @@
+#include "MyString.hpp"
+
+int main() {
+	const MyString cs("Ein konstanter String");
+	MyString s(cs);
+	s.assign(cs);
+	s.append(cs);
+	cout << cs.c_str() << endl;
+	cout << cs.size() << endl;
+	cout << cs.capacity() << endl;
+	cout << boolalpha << cs.empty() << endl;
+	s = cs + cs;
+	cout << (cs == cs) << endl;
+	s = cs;
+	cout << cs << endl;
+	s.at(1) = 'X';
+	s[2] = 'Y';
+	cout << s << endl;
+	return 0;
+}
