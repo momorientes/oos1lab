61df6a46e42c8b628209405435ced9026dd3e2ae
Wolfram Ladurner <wolait01@hs-esslingen.de>
2015-06-10 10:55:56 +0200

    A3

--- Labore/Labor_4/A3/Vererbung.cpp
+++ Labore/Labor_4/A3/Vererbung.cpp
@@ -53,19 +53,19 @@ public:
 	A(int a = 0xA) : a(a) {}
 };
 
-class B : public A {
+class B : virtual public A {
 public:
 	int b; 
 	B(int a = 0xBA, int b = 0xB) : A(a), b(b) {}
 };
 
-class C1 : public B { 
+class C1 : virtual public B { 
 public:
 	C1(int a = 0xC1A, int b = 0xC1B) 
 		: B(a, b) {} 
 };
 
-class C2 : public B { 
+class C2 : virtual public B { 
 public:
 	C2(int a = 0xC2A, int b = 0xC2B) 
 		: B(a, b) {} 
@@ -81,7 +81,7 @@ class D1 : public C1, public C2 {
 public:
 	D1(int C1_a = 0xD1C1A, int C2_a = 0xD1C2A, 
 	   int C1_b = 0xD1C1B, int C2_b = 0xD1C2B) 
-		: C1(C1_a, C1_b), C2(C2_a, C2_b) {}                                                      
+		: C1(C1_a, C1_b), C2(C2_a, C2_b), A(0xD1A) {}                                                      
 	void print() {
 		cout << "D1::print() ..." << endl;
 		string strArr[] = 
@@ -96,7 +96,7 @@ class D2 : public C1, public C2, public C3 {
 public:
 	D2(int C1_a = 0xD2C1A, int C2_a = 0xD2C2A, int C3_a = 0xD2C3A, 
 	   int C1_b = 0xD2C1B, int C2_b = 0xD2C2B, int C3_b = 0xD2C3B) 
-		: C1(C1_a, C1_b), C2(C2_a, C2_b), C3(C3_a, C3_b) {}                              
+		: C1(C1_a, C1_b), C2(C2_a, C2_b), C3(C3_a, C3_b), B(0xA, 0xD2B) {}                              
 	void print() { 
 		cout << "D2::print() ..." << endl;
 		string strArr[] = { "C1::a", "C2::a", "C3::a", 
