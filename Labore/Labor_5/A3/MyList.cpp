6f337d60b0b363b7be92be1f19a8a6da0884f701
momo <github@mail.hackathon.de>
2015-06-22 17:34:47 +0200

    finished A3

--- Labore/Labor_5/A3/MyList.cpp
+++ Labore/Labor_5/A3/MyList.cpp
@@ -9,11 +9,30 @@ MyList::MyList()
 
 }
 
+MyList::MyList(const MyList& list)
+{
+    first = nullptr;
+    last = nullptr;
+    _size = 0;
+    MyListElement *tmp = list.first;
+    do {
+        this->push_back(*(tmp->data));
+        tmp = tmp->next;
+    } while(tmp);
+}
+
 void MyList::push_back(const MyData& data)
 {
 	MyList::MyListElement *element = new MyListElement();
     element->data = data.clone();
     element->next = nullptr;
+    if(first == nullptr)
+    {
+        first = element;
+    } else {
+        last->next = element;
+    }
+    last = element;
     _size++;
 }
 
@@ -33,17 +52,18 @@ void MyList::pop_back() {
 
 void MyList::clear() {
     MyList::MyListElement *tmp = first;
-    while(tmp->next)
-    {
-        MyList::MyListement *prev = tmp;
-        tmp = tmp->next;
+    do {
+        MyList::MyListElement *prev = tmp;
         delete prev;
-    }
+        tmp = tmp->next;
+    } while(tmp);
     _size = 0;
+    first = nullptr;
+    last = nullptr;
 }
 
 bool MyList::empty() const {
-	return _size > 0;
+	return _size == 0;
 }
 
 MyData& MyList::front() const {
@@ -55,14 +75,38 @@ MyData& MyList::back() const {
 }	
 
 void MyList::print() const {
-	std::cout << "asdfasdf" << std::endl;
+    if(empty()){
+        return;
+    }
+    MyListElement *tmp = first;
+    do {
+        tmp->data->print();
+        tmp = tmp->next;
+    } while(tmp);
+
 }
 
 int MyList::size() const {
 	return _size;
 }
 
-const MyList MyList::operator+(const MyList& l) const {
-    MyList ret = new MyList();
+MyList& MyList::operator=(const MyList &list) {
+    this->clear();
+    MyListElement *tmp = list.first;
+    do {
+        this->push_back(*(tmp->data));
+        tmp = tmp->next;
+    } while(tmp);
+    return *this;
+}
 
+const MyList MyList::operator+(const MyList& l) const {
+    MyList *ret = new MyList(*this); 
+
+    MyListElement *tmp = l.first;
+    do { 
+        ret->push_back(*tmp->data);
+        tmp = tmp->next;
+    } while(tmp);
+    return *ret;
 }
